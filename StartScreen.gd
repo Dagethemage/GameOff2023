98e2e2f5518631ffbbf18efad78681192f3591e5
Dagethemage <88294578+Dagethemage@users.noreply.github.com>
2023-11-02 12:02:55 -0500

    Added Start menu
    Start menu with keyboard and controller support

---
+++ StartScreen.gd
@@ -0,0 +1,21 @@
+extends CenterContainer
+
+@onready var start = $MarginContainer/VBoxContainer/Start
+@onready var options = $MarginContainer/VBoxContainer/Options
+@onready var quit = $MarginContainer/VBoxContainer/Quit
+
+
+func _ready() -> void:
+	start.grab_focus()
+	start.pressed.connect(start_pressed)
+	options.pressed.connect(options_pressed)
+	quit.pressed.connect(quit_pressed)
+
+func start_pressed():
+	get_tree().change_scene_to_file("res://Levels/World.tscn")
+
+func options_pressed():
+	pass
+
+func quit_pressed():
+	get_tree().quit()
