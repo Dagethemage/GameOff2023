efd4f90817c04da6ce0469abae0cb4494db6ed76
Dagethemage <88294578+Dagethemage@users.noreply.github.com>
2024-03-25 09:56:01 -0500

    Level 3 World 1

--- GUI/StartScreen.gd
+++ GUI/StartScreen.gd
@@ -36,7 +36,8 @@ func start_pressed():
 	LevelTranstition.fade_to_black()
 	await LevelTranstition.fade_to_black()
 	await get_tree().create_timer(2).timeout
-	get_tree().change_scene_to_file("res://Levels/hub_world.tscn")
+	LevelReferences.goto_scene("res://Levels/hub_world.tscn")
+	#get_tree().change_scene_to_file("res://Levels/hub_world.tscn")
 	LevelTranstition.fade_from_black()
 
 func quit_pressed():
